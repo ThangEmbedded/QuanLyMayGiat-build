c64fcf3c4198b2ef53690418327252aef22f0ddd
Thanghd99 <thanghd@elifetech.vn>
2026-06-28 13:42:52 +0700

    init: clear src before init project

--- build/CMakeFiles/LaundryKiosk.dir/LaundryKiosk_autogen/UVLADIE3JM/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/LaundryKiosk.dir/LaundryKiosk_autogen/UVLADIE3JM/qrc_resources.cpp.o: \
- /home/toppi/work_data/1_projects/QuanLyMayGiat/build/LaundryKiosk_autogen/UVLADIE3JM/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/LaundryKiosk_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/LaundryKiosk_autogen"
-  "LaundryKiosk_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/LaundryKiosk_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
